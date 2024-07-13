187667488356a03d511eeb4d6e3924cd040ce0d4
Zain Ul Abideen <127882169+RDX-1@users.noreply.github.com>
2024-07-13 19:11:35 +0500

    latest commit

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
-import Particle from "../Particle";
+// import Particle from "../Particle";
 
 function contact() {
   return (
